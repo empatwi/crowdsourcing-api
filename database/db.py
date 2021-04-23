7582d0ec6da8fbd0972e1bf8d6ad6a569d3d29c3
Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
2021-04-23 17:01:03 -0300

    Random tweet in every GET request

--- database/db.py
+++ database/db.py
@@ -1,10 +1,6 @@
 import pymongo
-import json
-
 from decouple import config
-from bson.json_util import dumps
 
 mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING'), connect=False)
 db = pymongo.database.Database(mongo, 'empatwi')
 col = pymongo.collection.Collection(db, 'tweet')
-col_results = json.loads(dumps(col.find()))
--- routes.py
+++ routes.py
@@ -1,18 +1,27 @@
+import pymongo
+import json
+
 from api import api
-from database.db import col_results
+from bson.json_util import dumps
+from database.db import col
 from flask_restplus import Resource, fields, Namespace
 from flask import request, render_template, make_response, jsonify
 
 tweet_ns = Namespace('tweet', description='Tweet related operations')
 
 # Model required by flask_restplus for expected data
+classification_fields = api.model('Classification', {
+    'created_at': fields.DateTime(required=True),
+    'classification': fields.Boolean(required=True)
+})
+
 tweet_fields = api.model('Tweet', {
     'created_at': fields.DateTime(required=True),
     'tweet_content': fields.String(required=True),
     'user_location': fields.String(required=False),
     'keyword': fields.List(fields.String(), required=True),
     'entities': fields.List(fields.String(), required=False),
-    #classification: fields.List([created_at, bool], required=True)
+    'classification': fields.List(fields.Nested(classification_fields))
 })
 
 @tweet_ns.route('/')
@@ -24,4 +33,12 @@ class Tweet(Resource):
     })
     @tweet_ns.doc(description='Endpoint to retrieve all tweets')
     def get(self):
-        return col_results, 200
+        return json.loads(dumps(col.aggregate([{
+            '$project': {
+                '_id': 0,
+                'created_at': 0,
+                'keyword': 0,
+                'user_location': 0,
+                'entities': 0
+            }
+        }, {'$sample': {'size': 1}}]))), 200
