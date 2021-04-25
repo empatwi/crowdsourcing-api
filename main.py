388f0b8ec718ab7a0f81f6ecebc2c1bd6d62577d
Fabiana <39682297+fabianamasini@users.noreply.github.com>
2021-04-25 12:05:07 -0300

    CORS First configurations (#4)
    * Updated GET query
    
    * Updating CORS policy

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from api import bp, api
+from flask_cors import CORS, cross_origin
 from flask import Flask
 from routes import tweet_ns
 
 app = Flask(__name__, instance_relative_config=True)
+cors = CORS(app)
+
+cross_origin(["http://localhost:3000"])
+
 app.register_blueprint(bp)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config.from_mapping(SECRET_KEY='dev')
