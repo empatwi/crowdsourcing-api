b2c17d05feeeaf1e01bfbdb898fd06c5f42452e9
Fabiana <39682297+fabianamasini@users.noreply.github.com>
2021-04-27 22:43:31 -0300

    Fixed CORS API policies and authorization (#5)
    * Updated GET query
    
    * Updating CORS policy
    
    * Updating CORS
    
    * Updated to dev database and CORS policies
    
    * Updated CORS
    
    * Resolved OPTIONS issue
    
    * Added headers to CORS to perform PUT request successfully
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ from flask_cors import CORS, cross_origin
 from flask import Flask
 from routes import tweet_ns
 
-app = Flask(__name__, instance_relative_config=True)
-cors = CORS(app)
+config = {
+    'ORIGINS': [
+        'http://localhost:3000/'
+    ]
+}
 
-cross_origin(["http://localhost:3000"])
+app = Flask(__name__, instance_relative_config=True)
+cors = CORS(app, resources={'/*': {'origins': config['ORIGINS']}})
 
 app.register_blueprint(bp)
 app.config['CORS_HEADERS'] = 'Content-Type'
@@ -15,7 +19,16 @@ app.config['DEBUG'] = True
 app.config['RESTPLUS_VALIDATE'] = True
 app.config['SWAGGER_UI_DOC_EXPANSION'] = 'list'
 
+cross_origin(automatic_options=True)
+
 api.add_namespace(tweet_ns)
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'PUT')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    return response
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000, debug=True)
--- routes.py
+++ routes.py
@@ -1,9 +1,11 @@
 import json
+import logging
 
 from api import api
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from database.db import col
+from flask_cors import cross_origin
 from flask_restplus import Resource, fields, Namespace
 from flask import request
 
@@ -40,7 +42,7 @@ class Tweet(Resource):
             {'$sample': {'size': 1}}
         ])))
 
-@tweet_ns.route('/<id>')
+@tweet_ns.route('/<id>/')
 class TweetClassification(Resource):
     @tweet_ns.doc(responses={
         200: 'OK',
@@ -59,7 +61,6 @@ class TweetClassification(Resource):
                 {'_id': ObjectId(id)}, 
                 { '$push': {'classification': classification}}
             )))
-            return json.loads(dumps(col.find({'_id': ObjectId(id)})))
+            return json.loads(dumps(col.find({'_id': ObjectId(id)}))), 200
         else:
             return 'Tweet not found', 404
-        
