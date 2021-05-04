bd12ab27e68b84e181421b9d76445beb3a0dce47
Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
2021-05-04 14:00:28 -0300

    Updated MongoDB Atlas

--- database/db.py
+++ database/db.py
@@ -3,7 +3,7 @@ from decouple import config
 
 mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING_DEVELOPMENT'), connect=False)
 #Dev db
-db = pymongo.database.Database(mongo, 'empatwi')
+db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_DEV_NAME'))
 #Test db
-#db = pymongo.database.Database(mongo, 'test')
-col = pymongo.collection.Collection(db, 'tweet')
+#db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_TEST_NAME'))
+col = pymongo.collection.Collection(db, config('MONGODB_ATLAS_COLLECTION_NAME'))
