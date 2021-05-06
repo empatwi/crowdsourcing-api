b3e8cef47db13eb963262b6aeea221cc8e5f3957
Fabiana Masini <masinifabiana@gmail.com>
2021-05-06 08:25:39 -0300

    Updated Connection String

--- database/db.py
+++ database/db.py
@@ -1,10 +1,6 @@
 import pymongo
 from decouple import config
 
-#Dev db
-mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING_DEVELOPMENT'), connect=False)
-db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_DEV_NAME'))
-#Prod db
-# mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING_PROD'), connect=False)
-# db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_PROD_NAME'))
+mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING_PROD'), connect=False)
+db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_PROD_NAME'))
 col = pymongo.collection.Collection(db, config('MONGODB_ATLAS_COLLECTION_NAME'))
