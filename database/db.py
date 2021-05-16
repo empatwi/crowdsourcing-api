452a28e57271cd5cb21f11a156a73e326b033b06
Fabiana Masini <masinifabiana@gmail.com>
2021-05-15 23:25:36 -0300

    Reported endpoint. TODO: Match query not working

--- database/db.py
+++ database/db.py
@@ -1,6 +1,6 @@
 import pymongo
 from decouple import config
 
-mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING_PROD'), connect=False)
-db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_PROD_NAME'))
+mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING_TEST'), connect=False)
+db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_TEST_NAME'))
 col = pymongo.collection.Collection(db, config('MONGODB_ATLAS_COLLECTION_NAME'))
--- routes.py
+++ routes.py
@@ -3,7 +3,6 @@ import json
 from api import api
 from bson.json_util import dumps
 from bson.objectid import ObjectId
-from utils.custom_fields import NullableBoolean
 from database.db import col
 from flask_restplus import Resource, fields, Namespace
 from flask import request
@@ -13,9 +12,12 @@ tweet_ns = Namespace('tweet', description='Tweet related operations')
 # Model required by flask_restplus for expected data
 classification_fields = api.model('Classification', {
     'created_at': fields.DateTime(required=True),
-    #'classification': fields.Boolean(required=True)
-    'classification': NullableBoolean(),
-    'reported': NullableBoolean()
+    'classification': fields.Boolean(required=True)
+})
+
+reported_fields = api.model('Reported', {
+    'reported_at': fields.DateTime(required=True),
+    'reported': fields.Boolean(required=True)
 })
 
 tweet_fields = api.model('Tweet', {
@@ -33,9 +35,10 @@ class Tweet(Resource):
     @tweet_ns.doc(description='Retrieves a random tweet')
     def get(self):
         return json.loads(dumps(col.aggregate([
-            {'$match': {
-                'classification.2': {'$exists': False}
-            }}, 
+            {'$match': { '$or': [
+                {'classification.2': {'$exists': False}},
+                {'reported.2': {'$exists': False}}
+            ]}}, 
             {'$project': {
                 '_id': {'$toString': '$_id'},
                 'tweet_content': 1
@@ -55,22 +58,34 @@ class TweetClassification(Resource):
     @tweet_ns.doc(description='Sends classification data to database')
     def put(self, id):
         unclassified_tweet = json.loads(dumps(col.find({'_id': ObjectId(id)})))
-
         if unclassified_tweet:
             classification = request.get_json()
-            if classification['classification'] is not None:
-                json.loads(dumps(col.update(
-                    {'_id': ObjectId(id)}, 
-                    { '$push': {
-                        'created_at': classification['created_at'],
-                        'classification': classification['classification']
-                    }}
-                )))
-            elif classification['reported'] is not None:
-                json.loads(dumps(col.update(
-                    {'_id': ObjectId(id)},
-                    {'$push': {}}
-                )))
+            json.loads(dumps(col.update(
+                {'_id': ObjectId(id)}, 
+                {'$push': {'classification': classification}}
+            )))
+            return json.loads(dumps(col.find({'_id': ObjectId(id)}))), 200
+        else:
+            return 'Tweet not found', 404        
+
+@tweet_ns.route('/reported/<id>/')
+class TweetReported(Resource):
+    @tweet_ns.doc(responses={
+        200: 'OK',
+        400: 'Bad request',
+        404: 'Tweet not found',
+        500: 'Internal server error'
+    })
+    @tweet_ns.expect(reported_fields, validate=True)
+    @tweet_ns.doc(description='Sends reported tweet data to database')
+    def put(self, id):
+        reported_tweet = json.loads(dumps(col.find({'_id': ObjectId(id)})))
+        if reported_tweet:
+            reported = request.get_json()
+            json.loads(dumps(col.update(
+                {'_id': ObjectId(id)},
+                {'$push': {'reported': reported}}
+            )))
             return json.loads(dumps(col.find({'_id': ObjectId(id)}))), 200
         else:
             return 'Tweet not found', 404
