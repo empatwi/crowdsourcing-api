5dd4b8acf1ad8d0cc5dc390bf7aafe84efccc62d
Fabiana <39682297+fabianamasini@users.noreply.github.com>
2021-11-08 10:14:54 -0300

    Second release (#11)
    * Moving back-end to a separate repository
    
    * API endpoints (#1)
    
    * Implemented connection to MongoDB Atlas database and collections and Swagger for better API management
    
    * Random tweet in every GET request
    
    * Reestructured project
    
    * Using test atlas database
    
    * Started PUT Request to update classification
    
    * Pushes classification data to DB
    
    * PUT endpoint with query
    
    * Updated README.md
    
    * Added Procfile
    
    * Moved main.py to __init__
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
    
    * Procfile configurations (#2)
    
    * Implemented connection to MongoDB Atlas database and collections and Swagger for better API management
    
    * Random tweet in every GET request
    
    * Reestructured project
    
    * Using test atlas database
    
    * Started PUT Request to update classification
    
    * Pushes classification data to DB
    
    * PUT endpoint with query
    
    * Updated README.md
    
    * Added Procfile
    
    * Moved main.py to __init__
    
    * Updating to procfile
    
    * Reestructured project to Heroku deploy
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
    
    * Procfile update (#3)
    
    * Implemented connection to MongoDB Atlas database and collections and Swagger for better API management
    
    * Random tweet in every GET request
    
    * Reestructured project
    
    * Using test atlas database
    
    * Started PUT Request to update classification
    
    * Pushes classification data to DB
    
    * PUT endpoint with query
    
    * Updated README.md
    
    * Added Procfile
    
    * Moved main.py to __init__
    
    * Updating to procfile
    
    * Reestructured project to Heroku deploy
    
    * Procfile
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
    
    * CORS First configurations (#4)
    
    * Updated GET query
    
    * Updating CORS policy
    
    * Fixed CORS API policies and authorization (#5)
    
    * Updated GET query
    
    * Updating CORS policy
    
    * Updating CORS
    
    * Updated to dev database and CORS policies
    
    * Updated CORS
    
    * Resolved OPTIONS issue
    
    * Added headers to CORS to perform PUT request successfully
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
    
    * MongoDB Atlas Update (#6)
    
    * Implemented connection to MongoDB Atlas database and collections and Swagger for better API management
    
    * Random tweet in every GET request
    
    * Reestructured project
    
    * Using test atlas database
    
    * Started PUT Request to update classification
    
    * Pushes classification data to DB
    
    * PUT endpoint with query
    
    * Updated README.md
    
    * Added Procfile
    
    * Moved main.py to __init__
    
    * Updating to procfile
    
    * Reestructured project to Heroku deploy
    
    * Procfile
    
    * Updated MongoDB Atlas
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
    
    * Final adjusts to first release (#7)
    
    * Updated Connection String
    
    * Update README.md
    
    * Feature/report abuse (#9)
    
    * Created custom field for nullable boolean
    
    * Reported endpoint. TODO: Match query not working
    
    * Reported query
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
    
    * Feature/report abuse (#10)
    
    * Created custom field for nullable boolean
    
    * Reported endpoint. TODO: Match query not working
    
    * Reported query
    
    * Fixed query
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
    
    Co-authored-by: Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>

--- routes.py
+++ routes.py
@@ -15,6 +15,11 @@ classification_fields = api.model('Classification', {
     'classification': fields.Boolean(required=True)
 })
 
+reported_fields = api.model('Reported', {
+    'reported_at': fields.DateTime(required=True),
+    'reported': fields.Boolean(required=True)
+})
+
 tweet_fields = api.model('Tweet', {
     'tweet_content': fields.String(required=True),
     'classification': fields.List(fields.Nested(classification_fields))
@@ -29,15 +34,17 @@ class Tweet(Resource):
     })
     @tweet_ns.doc(description='Retrieves a random tweet')
     def get(self):
-        return json.loads(dumps(col.aggregate([
-            {'$match': {
-                'classification.2': {'$exists': False}
-            }}, 
-            {'$project': {
-                '_id': {'$toString': '$_id'},
-                'tweet_content': 1
-            }},
-            {'$sample': {'size': 1}}
+        return json.loads(dumps(col.aggregate(
+            [
+                {'$match': { '$and': [
+                    {'classification.2': {'$exists': False}},
+                    {'reported.2': { '$exists': False }}
+                ]}}, 
+                {'$project': {
+                    '_id': {'$toString': '$_id'},
+                    'tweet_content': 1
+                }},
+                {'$sample': {'size': 1}}
         ])))
 
 @tweet_ns.route('/<id>/')
@@ -52,12 +59,33 @@ class TweetClassification(Resource):
     @tweet_ns.doc(description='Sends classification data to database')
     def put(self, id):
         unclassified_tweet = json.loads(dumps(col.find({'_id': ObjectId(id)})))
-
         if unclassified_tweet:
             classification = request.get_json()
             json.loads(dumps(col.update(
                 {'_id': ObjectId(id)}, 
-                { '$push': {'classification': classification}}
+                {'$push': {'classification': classification}}
+            )))
+            return json.loads(dumps(col.find({'_id': ObjectId(id)}))), 200
+        else:
+            return 'Tweet not found', 404        
+
+@tweet_ns.route('/reported/<id>/')
+class TweetReported(Resource):
+    @tweet_ns.doc(responses={
+        200: 'OK',
+        400: 'Bad request',
+        404: 'Tweet not found',
+        500: 'Internal server error'
+    })
+    @tweet_ns.expect(reported_fields, validate=True)
+    @tweet_ns.doc(description='Sends reported tweet data to database')
+    def put(self, id):
+        reported_tweet = json.loads(dumps(col.find({'_id': ObjectId(id)})))
+        if reported_tweet:
+            reported = request.get_json()
+            json.loads(dumps(col.update(
+                {'_id': ObjectId(id)},
+                {'$push': {'reported': reported}}
             )))
             return json.loads(dumps(col.find({'_id': ObjectId(id)}))), 200
         else:
