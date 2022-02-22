2beeab400c21760d34ced8160673d8f65e0ac498
Fabiana Garcia <fabiana.garcia@fit-tecnologia.org.br>
2022-02-22 09:16:46 -0300

    Changed test to develop db

--- database/db.py
+++ database/db.py
@@ -1,6 +1,6 @@
 import pymongo
 from decouple import config
 
-mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING_TEST'), connect=False)
-db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_TEST_NAME'))
+mongo = pymongo.MongoClient(config('MONGODB_ATLAS_CONNECTION_STRING_DEVELOPMENT'), connect=False)
+db = pymongo.database.Database(mongo, config('MONGODB_ATLAS_DEV_NAME'))
 col = pymongo.collection.Collection(db, config('MONGODB_ATLAS_COLLECTION_NAME'))
